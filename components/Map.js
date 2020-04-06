8873ede53f83f25bb4c09c8b83e6c7f5b5801407
andresmichel <andresmichelg@gmail.com>
2020-04-05 18:57:13 -0700

    add mapbox

---
+++ components/Map.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import ReactMapGL from 'react-map-gl';
+
+const TOKEN = 'pk.eyJ1IjoiYW5kcmVzbWljaGVsIiwiYSI6ImNqa3k2NGFsbzBmODYza21tcWo2NjdlcGUifQ.txk49vcGtXSpKyQOnPqiOQ'
+
+export default function Map() {
+    const [viewport, setViewport] = useState({
+        width: '100vw',
+        height: '100vh',
+        latitude: 37.7577,
+        longitude: -122.4376,
+        zoom: 8
+    });
+
+    return (
+        <ReactMapGL
+            mapboxApiAccessToken={TOKEN}
+            {...viewport}
+            onViewportChange={setViewport}
+        />
+    );
+}
