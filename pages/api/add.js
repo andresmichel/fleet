80d0bf4bbde3b4f83fc619ddcd15bd286fdfe56f
andresmichel <andresmichelg@gmail.com>
2020-04-07 22:15:16 -0700

    use dynamodb

---
+++ pages/api/add.js
@@ -0,0 +1,35 @@
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    region: 'us-east-1',
+});
+
+module.exports = (req, res) => {
+    const crypto = require('crypto');
+    const { lat, lng } = req.body.lat;
+    const docClient = new AWS.DynamoDB.DocumentClient();
+    const params = {
+        TableName: process.env.TABLE_NAME,
+        Item: {
+            id: crypto.createHash('md5').update(`${lat},${lng}`).digest('hex'),
+            lat,
+            lng,
+        }
+    };
+    docClient.put(params, function (err, data) {
+        if (err) {
+            res.json({
+                success: false,
+                message: 'Error: Server error'
+            });
+        } else {
+            res.json({
+                success: true,
+                message: 'Loaded item',
+                data
+            });
+        }
+    });
+}
---
+++ pages/api/get.js
@@ -0,0 +1,29 @@
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    region: 'us-east-1',
+});
+
+module.exports = (_, res) => {
+    const docClient = new AWS.DynamoDB.DocumentClient();
+    const params = {
+        TableName: process.env.TABLE_NAME
+    };
+    docClient.scan(params, function (err, data) {
+        if (err) {
+            res.json({
+                success: false,
+                message: 'Error: Server error'
+            });
+        } else {
+            const { Items } = data;
+            res.json({
+                success: true,
+                message: 'Loaded items',
+                items: Items
+            });
+        }
+    });
+}
