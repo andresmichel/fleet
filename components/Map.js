912f53f11f6f4fa3868bd32ad65287b98686e72c
andresmichel <andresmichelg@gmail.com>
2020-04-23 18:48:31 -0700

    show markers

--- components/Map.js
+++ components/Map.js
@@ -1,4 +1,4 @@
-import React, { useState, } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactMapGL from 'react-map-gl';
 import Marker from '../components/Marker';
 
@@ -10,8 +10,27 @@ export default function Map() {
         height: '100vh',
         latitude: 37.7577,
         longitude: -122.4376,
-        zoom: 12
+        zoom: 15
     });
+    const [markers, setMarkers] = useState([]);
+    async function fetchLocations() {
+        try {
+            const response = await fetch('/api/get');
+            let { items } = await response.json();
+            items = items.slice(items.length - 1, items.length);
+            setMarkers(items);
+            setViewport({ ...viewport, latitude: items[0].latitude, longitude: items[0].longitude })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    function fetchLocationsEvery() {
+        fetchLocations()
+        setTimeout(fetchLocationsEvery, 5000);
+    }
+    useEffect(() => {
+        fetchLocationsEvery();
+    }, []);
     return (
         <ReactMapGL
             mapStyle="mapbox://styles/mapbox/dark-v9"
@@ -19,18 +38,14 @@ export default function Map() {
             onViewportChange={setViewport}
             {...viewport}
         >
-            <Marker
-                latitude={37.7577}
-                longitude={-122.4376}
-                color={'red'} />
-            <Marker
-                latitude={37.7677}
-                longitude={-122.4376}
-                color={'green'} />
-            <Marker
-                latitude={37.7777}
-                longitude={-122.4376}
-                color={'blue'} />
+            {markers.map((marker, index) => (
+                <Marker
+                    key={index}
+                    latitude={marker.latitude}
+                    longitude={marker.longitude}
+                    deviceId={marker.deviceId}
+                    color={'skyblue'} />
+            ))}
         </ReactMapGL>
     );
 }
--- components/Marker.js
+++ components/Marker.js
@@ -2,25 +2,26 @@ import React, { useState, useEffect } from 'react';
 import { Marker } from 'react-map-gl';
 
 export default function MovingMarker(props) {
-    const [location, setLocation] = useState(getLocation());
-    function getLocation() {
-        return {
-            latitude: props.latitude + Math.random() * 0.01,
-            longitude: props.longitude + Math.random() * 0.01,
-        };
-    }
-    const updateLocation = () => {
-        setLocation(getLocation())
-        setTimeout(updateLocation, 5000)
-    }
-    useEffect(() => {
-        updateLocation();
-    }, []);
+    // const [location, setLocation] = useState(getLocation());
+    // function getLocation() {
+    //     return {
+    //         latitude: props.latitude + Math.random() * 0.01,
+    //         longitude: props.longitude + Math.random() * 0.01,
+    //     };
+    // }
+    // const updateLocation = () => {
+    //     setLocation(getLocation())
+    //     setTimeout(updateLocation, 5000)
+    // }
+    // useEffect(() => {
+    //     getLocation();
+    // }, []);
     return (
         <Marker
             offsetLeft={-10}
             offsetTop={-10}
-            {...location}>
+            latitude={props.latitude}
+            longitude={props.longitude}>
             <div style={{
                 position: 'relative',
                 height: 20,
